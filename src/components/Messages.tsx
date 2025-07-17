2b320ab2f88b630275ede816be74bd3b7e2d1070
localhy <localhy@proton.me>
2025-07-17 19:46:57 +0900

    Updated Messages.tsx

--- src/components/Messages.tsx
+++ src/components/Messages.tsx
@@ -398,10 +398,12 @@ const Messages = () => {
                             style={{ fontFamily: 'Inter' }}
                           >
                             {selectedConversation.other_user.name}
-                          </h3>                          
+                          </h3>
+                          {/* <p className="text-sm text-gray-500">Online</p> */}
                         </div>
                       </div>
-                      
+
+                      {/*
                       <div className="flex items-center space-x-2">
                         <button className="text-gray-500 hover:text-gray-700 p-2 rounded-lg hover:bg-gray-100">
                           <Phone className="h-5 w-5" />
@@ -414,6 +416,7 @@ const Messages = () => {
                         </button>
                       </div>
                     </div>
+                    */}
 
                     {/* Messages */}
                     <div className="flex-1 overflow-y-auto p-4 space-y-4">
